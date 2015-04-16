7a604095aff8e35531754abb92c794b06f5eb25f
Julius <tomsworld@gmx.net>
2015-04-16 09:33:45 +0200

    FIX update required extensions for testing, removed redundant tests

--- tests/EmbargoExpiryTest.php
+++ tests/EmbargoExpiryTest.php
@@ -11,8 +11,8 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 	 */
 	protected $requiredExtensions = array(
 		'SiteTree' => array(
-			'EmbargoExpiryExtension',
-			'Versioned',
+			'WorkflowEmbargoExpiryExtension',
+			'Versioned'
 		)
 	);
 
@@ -21,7 +21,7 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 	 */
 	protected $illegalExtensions = array(
 		'SiteTree' => array(
-			"Translatable",
+			'Translatable',
 		)
 	);
 
@@ -66,50 +66,5 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 
 		$this->assertTrue($page->PublishJobID == 0);
 		$this->assertTrue($page->UnPublishJobID == 0);
-	}
-
-	public function testPublishActionWithFutureDates() {
-		$action = new PublishItemWorkflowAction();
-		$instance = new WorkflowInstance();
-
-		$page = new Page();
-		$page->Title = 'stuff';
-		$page->DesiredPublishDate = '2020-02-01 00:00:00';
-		$page->DesiredUnPublishDate = '2020-02-01 02:00:00';
-
-		$page->write();
-
-		$instance->TargetClass = $page->ClassName;
-		$instance->TargetID = $page->ID;
-
-		$action->execute($instance);
-
-		$page = DataObject::get_by_id('Page', $page->ID);
-		$this->assertTrue($page->PublishJobID > 0);
-		$this->assertTrue($page->UnPublishJobID > 0);
-	}
-
-	protected function createDefinition() {
-		$definition = new WorkflowDefinition();
-		$definition->Title = "Dummy Workflow Definition";
-		$definition->write();
-
-		$stepOne = new WorkflowAction();
-		$stepOne->Title = "Step One";
-		$stepOne->WorkflowDefID = $definition->ID;
-		$stepOne->write();
-
-		$stepTwo = new WorkflowAction();
-		$stepTwo->Title = "Step Two";
-		$stepTwo->WorkflowDefID = $definition->ID;
-		$stepTwo->write();
-
-		$transitionOne = new WorkflowTransition();
-		$transitionOne->Title = 'Step One T1';
-		$transitionOne->ActionID = $stepOne->ID;
-		$transitionOne->NextActionID = $stepTwo->ID;
-		$transitionOne->write();
-
-		return $definition;
 	}
 }
